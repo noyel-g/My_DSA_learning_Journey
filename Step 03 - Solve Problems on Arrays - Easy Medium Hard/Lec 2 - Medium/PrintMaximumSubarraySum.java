55149354a76d5f345a1f3758f8805c3c2a5193c4
Noyel_George <noyel.george26@gmail.com>
2026-07-30 20:29:33 +0530

    Array-medium

---
+++ Step 03 - Solve Problems on Arrays - Easy Medium Hard/Lec 2 - Medium/PrintMaximumSubarraySum.java
@@ -0,0 +1,30 @@
+public class PrintMaximumSubarraySum {
+    public static void main(String[] args) {
+       int [] arr={-1,-1,1,0,1};//array must have majority element else it will fail
+      maxSubArray(arr);      
+    }
+
+    public static void maxSubArray(int[] nums) {
+          int len=nums.length;
+          int currsum=nums[0];
+          int maxsum=nums[0];
+          int start=0,end=0,temp=0;
+            for(int i=1;i<len;i++){
+              if(nums[i]>currsum+nums[i]){
+                  temp=i;
+                  currsum=nums[i];
+              }
+              else currsum=currsum+nums[i];
+
+              if(currsum>maxsum){
+                start=temp;
+                end=i;
+                maxsum=currsum;
+              }
+            }  
+        
+        for (int i=start;i<=end;i++) {
+          System.out.print(nums[i]+" ");
+        }
+    }
+}
